91cfdfbde5e1a8ba0818ac6da1e3ef041b8c65b0
Унжакова Екатерина <katerina.unz@gmail.com>
2021-12-21 00:01:46 +0700

    Переписан компонент формы на хуки

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,71 +1,143 @@
-import React, { Component } from 'react';
-// import PropTypes from "prop-types";
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import './ContactForm.scss';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  inputNameId = uuidv4();
-  inputNumberId = uuidv4();
+  const inputNameId = uuidv4();
+  const inputNumberId = uuidv4();
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
-  handleSubmit = e => {
+
+  const handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
-    this.props.onSubmit(this.state);
-    this.reset();
+
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit} className="PhonebookForm">
-        <label htmlFor={this.inputNameId}>Name</label>
-        <input
-          id={this.inputNameId}
-          value={name}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-          required
-          onChange={this.handleChange}
-        />
-        <label htmlFor={this.inputNumberId}>Number</label>
-        <input
-          id={this.inputNumberId}
-          value={number}
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-          required
-          onChange={this.handleChange}
-        />
-        <button
-          className="Phonebook__btn"
-          type="submit"
-          disabled={!name || !number}
-        >
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit} className="PhonebookForm">
+      <label htmlFor={inputNameId}>Name</label>
+      <input
+        id={inputNameId}
+        value={name}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        required
+        onChange={handleChange}
+      />
+      <label htmlFor={inputNumberId}>Number</label>
+      <input
+        id={inputNumberId}
+        value={number}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+        required
+        onChange={handleChange}
+      />
+      <button
+        className="Phonebook__btn"
+        type="submit"
+        disabled={!name || !number}
+      >
+        Add contact
+      </button>
+    </form>
+  );
 }
 
-export default ContactForm;
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   inputNameId = uuidv4();
+//   inputNumberId = uuidv4();
+
+//   handleChange = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     console.log(this.state);
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+//       <form onSubmit={this.handleSubmit} className="PhonebookForm">
+//         <label htmlFor={this.inputNameId}>Name</label>
+//         <input
+//           id={this.inputNameId}
+//           value={name}
+//           type="text"
+//           name="name"
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//           required
+//           onChange={this.handleChange}
+//         />
+//         <label htmlFor={this.inputNumberId}>Number</label>
+//         <input
+//           id={this.inputNumberId}
+//           value={number}
+//           type="tel"
+//           name="number"
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+//           required
+//           onChange={this.handleChange}
+//         />
+//         <button
+//           className="Phonebook__btn"
+//           type="submit"
+//           disabled={!name || !number}
+//         >
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default ContactForm;
